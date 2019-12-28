88d5bac96bc21a689f268983e18d5a4f9522a070
aaroncaffrey <aaroncaffrey@gmail.com>
2019-12-28 21:48:46 +0000

    initial commit

---
+++ ConsoleApp31/ss_types.cs
@@ -0,0 +1,13 @@
+﻿namespace Coil_DHC_Dataset_Maker
+{
+    public partial class feature_calcs
+    {
+        public enum ss_types
+        {
+            DSSP3,
+            DSSP,
+            STRIDE3,
+            STRIDE
+        }
+    }
+}
---
+++ peptides_server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coil_DHC_Dataset_Maker;
+using Newtonsoft.Json;
+
+namespace peptides_server
+{
+    public static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var id = int.Parse(args[0]);
+            var seq = args[1];
+
+            var x = r_peptides.get_values(seq,id);
+
+            var y = new subsequence_classification_data.feature_info_container {feautre_info_list = x};
+
+            var z = subsequence_classification_data.feature_info_container.serialise_json(y);
+
+            Console.Write(z);
+
+            //Console.ReadLine();
+        }
+    }
+}
